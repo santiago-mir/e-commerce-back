f423058b10a02111a76cab825a53e08734901b36
Santiago <san_chan97@hotmail.com>
2025-03-12 14:38:54 -0300

    Update cors

--- pages/api/me/index.ts
+++ pages/api/me/index.ts
@@ -1,6 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { userData } from "types";
-import { authMiddleware, bodySchemaMiddleware } from "lib/middlewares";
+import {
+  authMiddleware,
+  bodySchemaMiddleware,
+  corsMiddleware,
+} from "lib/middlewares";
 import { updateUserData, getUserData } from "controllers/users";
 import methods from "micro-method-router";
 import { object, string } from "yup";
@@ -37,7 +41,9 @@ const rawHandler = methods({
 
 const protectedHandler = authMiddleware(rawHandler);
 
-export default methods({
-  get: protectedHandler,
-  patch: bodySchemaMiddleware(patchBodySchema, protectedHandler),
-});
+export default corsMiddleware(
+  methods({
+    get: protectedHandler,
+    patch: bodySchemaMiddleware(patchBodySchema, protectedHandler),
+  })
+);
