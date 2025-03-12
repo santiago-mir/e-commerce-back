d3ea63974207fecb623a72d1ea61e522fa4f3a60
Santiago <san_chan97@hotmail.com>
2025-03-12 15:58:07 -0300

    add cors file

---
+++ pages/api/cors.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
+
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export default async function corsHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const cors = Cors({
+    methods: ["GET", "POST", "PATCH", "OPTIONS", "HEAD"],
+  });
+
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
+  // Rest of the API logic
+  res.json({ message: "Hello Everyone!" });
+}
