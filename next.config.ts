2d5590c48b5838c0e39e477257aedfa9d01c709a
Santiago <san_chan97@hotmail.com>
2025-03-13 18:31:28 -0300

    Update cors config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
+// next.config.js
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// Ejemplo, si tenés otras configuraciones en tu next.config, agregá los siguiente a lo que ya tengas
+module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
-
-export default nextConfig;
